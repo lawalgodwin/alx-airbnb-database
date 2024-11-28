1984acaa250b5524b6d6e28167ddd8bca5fab581
lawalgodwin <godwinl200@gmail.com>
2024-11-28 18:32:00 +0100

    Retrieve all users and bookings including users who never booked and bookings not linked to any user

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -13,3 +13,18 @@ SELECT p.name, p.location, p.pricepernight, r.comment
 FROM properties AS p
 LEFT JOIN reviews AS r
 ON p.property_id = r.property_id\G;
+
+-- A query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT CONCAT(u.first_name, " ", u.last_name) AS names, u.email, b.booking_id, b.start_date, b.end_date
+FROM users AS u
+LEFT JOIN bookings AS b
+ON u.user_id = b.user_id
+UNION
+SELECT CONCAT(u.first_name, " ", u.last_name) AS names, u.email, b.booking_id, b.start_date, b.end_date
+FROm users AS u
+RIGHT JOIN bookings AS b
+ON u.user_id = b.user_id;
+
+-- NOTE: In mySQL, FULL OUTER is not supported directly
+-- But it is equivqlent to the result set of the Combination of LEFT JOIN and RIGHT JOIN, hence the UNION
+-- FULL OUTER JOIN = LEFT JOIN + RIGHT JOIN ==> (LEFT JOIN UNION RIGHT JOIN)
