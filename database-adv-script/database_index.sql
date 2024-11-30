582ab57d430c473a93fd99270de9f189f17a4c89
lawalgodwin <godwinl200@gmail.com>
2024-11-30 11:56:30 +0100

    Index the status and created_at columns of the bookings table

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -3,4 +3,11 @@
 USE airbnb;
 
 -- index the first_name and last_name columns on the users table
+-- DROP INDEX IF EXISTS `idx_names` ON users;
+-- CREATE INDEX IF NOT EXISTS `idx_names` ON users(first_name, last_name);
 CREATE INDEX idx_names ON users(first_name, last_name);
+
+-- index the status and created_at columns on the bookings table
+-- DROP INDEX IF EXISTS `idx_status_and_bookingdate` ON bookings;
+-- CREATE INDEX IF NOT EXISTS idx_status_and_bookingdate ON bookings(bookings.status, created_at);
+CREATE INDEX idx_status_and_bookingdate ON bookings(status, created_at);
